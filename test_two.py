c5ed04a9f3dab6652308341381d38276948f2a92
daniel anggrianto <danggrianto@50onred.com>
2015-11-02 14:26:05 -0500

    complete test

--- test_two.py
+++ test_two.py
@@ -7,9 +7,23 @@ driver.get(site_url)
 
 try:
     email = driver.find_element_by_css_selector('#email')
-    if email!=None:
+    if email is not None:
         print 'TEST SUCCESS: email found'
 except Exception as e:
     print 'TEST FAIL: email not found'
+
+try:
+    password = driver.find_element_by_css_selector('#password')
+    if password is not None:
+        print 'TEST SUCCESS: password found'
+except Exception as e:
+    print 'TEST FAIL: password not found'
+
+try:
+    submit_button = driver.find_element_by_css_selector('#login')
+    if submit_button is not None:
+        print 'TEST SUCCESS: submit button found'
+except Exception as e:
+    print 'TEST FAIL: submit button not found'
 
 driver.quit()
